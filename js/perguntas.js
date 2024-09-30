d3086419d38505b041a3aa41daa1d7fcdd484700
pradomanuu <168730277+pradomanuu@users.noreply.github.com>
2024-09-30 11:25:50 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,63 +1,34 @@
-export const perguntas = [
-    {
-        enunciado: "Você prefere nike ou adidas?",
-        alternativas: [
-            {
-                texto: "nike",
-                afirmacao: [
-                    "você tem Bom gosto",
-                    "você sabe que eu gostei da escolha"
-                    ],
-                proxima: 1,
-            },
-            {
-                texto: "adidas",
-                afirmacao: [
-                    "você é um bosta",
-                    "você não sabe escolher"
-                    ],
-                proxima: 2,
-            }           
-            
-        ]
-    },
-    {
-        enunciado: "principal dano causado pelo racismo",
-        alternativas: [
-            {
-                texto:"fortalece a autoestima das vitimas",
-                afirmacao: [
-                    "racismo enfraquece a autoestima, perpetuando sentimentos de inferioridade",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "contribui para a exclusão social e marginalização de grupos minoritarios ",
-                afirmacao: [
-                    "reforça a desigualdade e estereótipos discriminatórios",
-                    "afirmacao 2"
-                    ]
-            }
-        ]
-    },
-    {
-        enunciado: "você apoia racismo?",
-        alternativas: [
-            {
-                texto:"sim, eu apoio o movimento racista",
-                afirmacao: [
-                    "você realmente já ouviu  falar o que é o racismo?  A sua história e quantas milhares de pessoa foram prejudicadas e mortas? ",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto:"Não, sou contra o movimento racista",
-                afirmacao: [
-                    "que legal! concordamos que o racismo não é uma coisa boa ",
-                    "afirmacao 2"
-                    ]
-            }
-            
-        ]
-    },
-];
+function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = ""; 
+}
+
+mostraPergunta();
